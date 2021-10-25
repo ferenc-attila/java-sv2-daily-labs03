8b49ae50635e799f4a020aed04914d6de2766554
Attila <attila.ferenc.80@gmail.com>
2021-10-25 18:50:31 +0200

    Week03 day01 Students code cleaning

--- week03-labs-task/src/main/java/day01/Students.java
+++ week03-labs-task/src/main/java/day01/Students.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class Students {
 
-   private List<Integer> heights = new ArrayList<>();
+    private List<Integer> heights = new ArrayList<>();
 
-   public void addHeight(int height) {
-       heights.add(height);
-   }
+    public void addHeight(int height) {
+        heights.add(height);
+    }
 
-   public boolean isHeightIncreasing() {
-       for (int i = 1; i<heights.size(); i++) {
-           if (heights.get(i) < heights.get(i-1)) {
-                   return false;
-           }
-       }
-       return true;
-   }
+    public boolean isHeightIncreasing() {
+        for (int i = 1; i < heights.size(); i++) {
+            if (heights.get(i) < heights.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
