bec0fcd34c29cf6350991325c34ed87178533285
Attila <attila.ferenc.80@gmail.com>
2021-10-26 20:17:16 +0200

    Upper triangular matrix code cleaning

--- week03-labs-task/src/main/java/day02/Matrix.java
+++ week03-labs-task/src/main/java/day02/Matrix.java
@@ -1,2 +1,15 @@
-package day02;public class Matrix {
+package day02;
+
+public class Matrix {
+
+    public boolean isUpperTriangularMatrix(int[][] matrix) {
+        for (int i = 0; i <= matrix.length - 1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (matrix[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- week03-labs-task/src/main/java/day02/MatrixMain.java
+++ week03-labs-task/src/main/java/day02/MatrixMain.java
@@ -1,2 +1,15 @@
-package day02;public class MatrixMain {
+package day02;
+
+public class MatrixMain {
+
+    public static void main(String[] args) {
+
+        Matrix matrix = new Matrix();
+
+        int[][] upperTriangularMatrix = {{1, 2, 3, 4, 5}, {0, 2, 3, 4, 5}, {0, 0, 3, 4, 5}, {0, 0, 0, 4, 5}, {0, 0, 0, 0, 5}};
+        int[][] regularMatrix = {{1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}, {1, 2, 3, 4, 5}};
+
+        System.out.println(matrix.isUpperTriangularMatrix(upperTriangularMatrix));
+        System.out.println(matrix.isUpperTriangularMatrix(regularMatrix));
+    }
 }
