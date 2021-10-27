ab53380910ccca78fe4d4205055f9f578139863d
Attila <attila.ferenc.80@gmail.com>
2021-10-27 10:26:26 +0200

    day02.Matrix head of for cycle updated

--- week03-labs-task/src/main/java/day02/Matrix.java
+++ week03-labs-task/src/main/java/day02/Matrix.java
@@ -3,7 +3,7 @@ package day02;
 public class Matrix {
 
     public boolean isUpperTriangularMatrix(int[][] matrix) {
-        for (int i = 0; i <= matrix.length - 1; i++) {
+        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (matrix[i][j] != 0) {
                     return false;
